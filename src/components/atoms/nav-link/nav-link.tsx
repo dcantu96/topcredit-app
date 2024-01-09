43f98c2f71a5fb786cbf42930e7c2b4dbd583d0c
David Cantu <davidcantu966@gmail.com>
2024-01-09 13:24:55 -0600

    feat: add nav link

---
+++ src/components/atoms/nav-link/nav-link.tsx
@@ -0,0 +1,17 @@
+import { Link as RouterLink, type To } from "react-router-dom";
+
+interface LinkProps {
+  to: To;
+  children: React.ReactNode;
+}
+
+export const NavLink = ({ children, to }: LinkProps) => {
+  return (
+    <RouterLink
+      to={to}
+      className="text-sm font-semibold leading-6 text-gray-900"
+    >
+      {children}
+    </RouterLink>
+  );
+};
