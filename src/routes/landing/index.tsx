6b0017f6c96eb9a56a5e5a3db5ac5efc30d77b12
David Cantu <davidcantu966@gmail.com>
2024-01-09 13:25:34 -0600

    feat: new landing screen

--- src/routes/landing/index.tsx
+++ src/routes/landing/index.tsx
@@ -1,3 +1,195 @@
+import NavLink from "components/atoms/nav-link";
+import { NavLink as RouterLink } from "react-router-dom";
+import logoSmall from "../../images/logo_small.png";
+import { useState } from "react";
+import ButtonLink from "components/atoms/button-link";
+
 export const Landing = () => {
-  return <div>landing screen</div>;
+  const [showMobileSidebar, setShowMobileSidebar] = useState(false);
+  return (
+    <div className="bg-white">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Topcredit</span>
+              <img
+                className="h-8 w-auto"
+                src={logoSmall}
+                alt="topcredit-logo"
+              />
+            </a>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setShowMobileSidebar((prev) => !prev)}
+            >
+              <span className="sr-only">Abrir menu</span>
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            <NavLink to="/clients">Clientes</NavLink>
+            <NavLink to="/benefits">Beneficios</NavLink>
+            <NavLink to="/blog">Noticias</NavLink>
+            <NavLink to="/legal">Legal</NavLink>
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <NavLink to="/login">
+              Iniciar sesión <span aria-hidden="true">&rarr;</span>
+            </NavLink>
+          </div>
+        </nav>
+
+        {showMobileSidebar && (
+          <div className="lg:hidden" role="dialog" aria-modal="true">
+            <div className="fixed inset-0 z-50"></div>
+            <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="flex items-center justify-between">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Topcredit</span>
+                  <img
+                    className="h-8 w-auto"
+                    src={logoSmall}
+                    alt="topcredit-logo"
+                  />
+                </a>
+                <button
+                  type="button"
+                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                  onClick={() => setShowMobileSidebar((prev) => !prev)}
+                >
+                  <span className="sr-only">Cerrar menu</span>
+                  <svg
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    <RouterLink
+                      to="/clients"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Clientes
+                    </RouterLink>
+                    <RouterLink
+                      to="/benefits"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Beneficios
+                    </RouterLink>
+                    <RouterLink
+                      to="/blog"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Noticias
+                    </RouterLink>
+                    <RouterLink
+                      to="/legal"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Legal
+                    </RouterLink>
+                  </div>
+                  <div className="py-6">
+                    <RouterLink
+                      to="/login"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Iniciar sesión
+                    </RouterLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </header>
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          ></div>
+        </div>
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Anunciando nuestro colaborador mas reciente <b>Soriana.</b>{" "}
+              <a href="#" className="font-semibold text-indigo-600">
+                <span className="absolute inset-0" aria-hidden="true"></span>
+                Entérate <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Créditos para empleados, ¡Tasas Increíbles!
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Mereces crecer tu dinero 15% al año en la app y una tarjeta de
+              crédito sin anualidad
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <ButtonLink to="register" size="md">
+                Inicia ahora
+              </ButtonLink>
+              <NavLink to="#">
+                Conoce más <span aria-hidden="true">→</span>
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
 };
