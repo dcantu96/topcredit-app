ae633a3b00fa5c982577d143340f588082c48457
David Cantu <davidcantu966@gmail.com>
2024-01-09 11:11:14 -0600

    fix: kitu types

--- src/components/providers/api/atoms.ts
+++ src/components/providers/api/atoms.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import Kitsu from "kitsu";
 import { selector } from "recoil";
 import { authState } from "../auth/atoms";
