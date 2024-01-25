3d556ea1eb750b155cc9d79d1cf6e256592feaf0
David Cantu <davidcantu966@gmail.com>
2024-01-25 13:56:39 -0600

    feat: update protected route

--- src/components/providers/auth/ProtectedRoute.tsx
+++ src/components/providers/auth/ProtectedRoute.tsx
@@ -1,14 +1,51 @@
 import { useRecoilValue } from "recoil";
-import { authState } from "./atoms";
+import { authState, myProfileState } from "./atoms";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-const ProtectedRoute = ({ children }: { children?: React.ReactNode }) => {
+interface ProtectedRouteProps {
+  children?: React.ReactNode;
+  /**
+   * specify which roles are allowed to access this route
+   * if not specified, all roles are allowed
+   * if specified, only the specified roles are allowed
+   *
+   * `admin` role is always allowed
+   */
+  allowedRoles?: string[];
+}
+
+const ProtectedRoute = ({ children, allowedRoles }: ProtectedRouteProps) => {
+  const navigate = useNavigate();
   const maybeAuth = useRecoilValue(authState);
+  const profile = useRecoilValue(myProfileState);
 
-  if (!maybeAuth) {
-    return <div>no auth</div>;
+  useEffect(() => {
+    // Redirect to login if not authenticated or profile is missing
+    if (!maybeAuth || !profile) {
+      navigate("/login");
+      return; // Early return to prevent further checks
+    }
+
+    // Check if profile and allowedRoles are valid for further checks
+    if (profile && allowedRoles) {
+      // Check if user role is not admin and either not in allowedRoles or role is undefined
+      const isNotAdmin = profile.role !== "admin";
+      const isRoleNotAllowed = profile.role
+        ? !allowedRoles.includes(profile.role)
+        : true;
+
+      if (isNotAdmin && isRoleNotAllowed) {
+        navigate("/not-allowed");
+      }
+    }
+  }, [maybeAuth, profile, navigate, allowedRoles]);
+
+  if (children) {
+    return <>{children}</>;
+  } else {
+    return <Outlet />;
   }
-
-  return <>{children}</>;
 };
 
 export default ProtectedRoute;
