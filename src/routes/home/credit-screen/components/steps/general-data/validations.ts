8fa51e9fd3402d1936cb5c27a5374b3fd823744f
David Cantu <davidcantu966@gmail.com>
2024-01-25 13:56:14 -0600

    fix: validations

--- src/routes/home/credit-screen/components/steps/general-data/validations.ts
+++ src/routes/home/credit-screen/components/steps/general-data/validations.ts
@@ -1,13 +1,13 @@
-export const postalCodeFieldValidation = (postalCode: string) => {
-  if (postalCode.length === 0) return "El código postal no puede estar vacío";
-  if (postalCode.length !== 5) return "El código postal debe tener 5 dígitos";
-  if (postalCode.match(/\D/))
-    return "El código postal solo puede contener números";
+export const postalCodeFieldValidation = (text: string) => {
+  if (text.length === 0) return "El código postal no puede estar vacío";
+  if (text.match(/\D/)) return "El código postal solo puede contener números";
+  if (text.length !== 5) return "El código postal debe tener 5 dígitos";
   return undefined;
 };
 
-export const bankAccountNumberFieldValidation = (bankAccountNumber: string) => {
-  if (bankAccountNumber.length === 0) return "CLABE no puede estar vacía";
-  if (bankAccountNumber.length !== 18) return "CLABE debe tener 18 dígitos";
+export const bankAccountNumberFieldValidation = (text: string) => {
+  if (text.length === 0) return "CLABE no puede estar vacía";
+  if (text.match(/\D/)) return "La CLABE solo puede contener números";
+  if (text.length !== 18) return "CLABE debe tener 18 dígitos";
   return undefined;
 };
