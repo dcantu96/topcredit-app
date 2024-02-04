86d112d55908bf0a2c67bbb4853a082b18c92303
David Cantu <davidcantu966@gmail.com>
2024-02-04 16:11:33 -0600

    Add useNavigate and useToast hooks to useRequestActions

--- src/routes/requests/actions.ts
+++ src/routes/requests/actions.ts
@@ -1,23 +1,78 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { useApi } from "components/providers/api/useApi"
+import useToast from "components/providers/toaster/useToast"
 
 export const useRequestActions = () => {
   const { id } = useParams()
+  const navigate = useNavigate()
+  const toast = useToast()
   const api = useApi()
   if (!id || Number.isNaN(id)) throw new Error("Missing user id")
 
   const approveUser = async () => {
-    const response = await api.update(`users`, { id, status: "hi" })
-    console.log(response)
+    try {
+      await api.update(`users`, { id, status: "approved" })
+      toast.success({
+        title: "Usuario actualizado",
+        message: "El usuario ha sido aprobado",
+      })
+      navigate("/requests")
+    } catch (error) {
+      let message = "Ocurrió un error al actualizar el usuario"
+      if (isJsonApiError(error)) message = error.errors[0].title
+      toast.error({
+        title: "Error al actualizar el usuario",
+        message,
+      })
+    }
   }
 
   const denyUser = async () => {
-    const response = await api.update(`users`, { id, status: "denied" })
-    console.log(response)
+    try {
+      await api.update(`users`, { id, status: "denied" })
+      toast.success({
+        title: "Usuario actualizado",
+        message: "El usuario ha sido denegado",
+      })
+      navigate("/requests")
+    } catch (error) {
+      let message = "Ocurrió un error al actualizar el usuario"
+      if (isJsonApiError(error)) message = error.errors[0].title
+      toast.error({
+        title: "Error al actualizar el usuario",
+        message,
+      })
+    }
   }
 
   return {
     approveUser,
     denyUser,
   }
+}
+
+export const isJsonApiError = (error: unknown): error is JsonApiError => {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "errors" in error &&
+    Array.isArray(error.errors)
+  )
+}
+
+export interface JsonApiError {
+  errors: JsonApiErrorItem[]
+}
+
+export interface JsonApiErrorItem {
+  title: string
+  detail: string
+  source: JsonApiErrorSource
+  code: string
+  status: string
+}
+
+export interface JsonApiErrorSource {
+  pointer: string
+  parameter?: string
 }
