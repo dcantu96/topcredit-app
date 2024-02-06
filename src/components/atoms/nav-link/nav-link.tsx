3f5a8e5ff1aca1089b53f93d44c682c7f21fbe60
David Cantu <davidcantu966@gmail.com>
2024-02-06 09:21:54 -0600

    Add variant prop to NavLink component

--- src/components/atoms/nav-link/nav-link.tsx
+++ src/components/atoms/nav-link/nav-link.tsx
@@ -1,13 +1,19 @@
 import { Link as RouterLink, type To } from "react-router-dom"
 
 interface LinkProps {
+  variant?: "dark"
   to: To
   children: React.ReactNode
 }
 
-export const NavLink = ({ children, to }: LinkProps) => {
+export const NavLink = ({ children, to, variant }: LinkProps) => {
   return (
-    <RouterLink to={to} className="text-sm font-medium leading-6 text-gray-900">
+    <RouterLink
+      to={to}
+      className={`text-sm font-medium leading-6 ${
+        variant === "dark" ? "text-gray-900" : "text-indigo-400"
+      }`}
+    >
       {children}
     </RouterLink>
   )
