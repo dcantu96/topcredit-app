f3be264b4bdd8199ee822901a79743d998491863
David Cantu <davidcantu966@gmail.com>
2024-02-23 12:02:57 -0600

    Refactor dashboard role-based rendering

--- src/routes/dashboards/index.tsx
+++ src/routes/dashboards/index.tsx
@@ -2,19 +2,19 @@ import { lazy } from "react"
 import { myProfileState } from "components/providers/auth/atoms"
 import { useRecoilValue } from "recoil"
 const Admin = lazy(() => import("./admin"))
-const StaffRequests = lazy(() => import("./staff-requests"))
+const StaffRequests = lazy(() => import("../../routes/requests/list"))
 
 /**
  * This component is in charge of rendering a screen depending on the user's role.
  */
 const Dashboard = () => {
-  const { roles } = useRecoilValue(myProfileState)
+  const profile = useRecoilValue(myProfileState)
 
-  if (roles.includes("admin")) {
+  if (profile?.roles.includes("admin")) {
     return <Admin />
-  } else if (roles.includes("requests")) {
+  } else if (profile?.roles.includes("requests")) {
     return <StaffRequests />
-  } else if (roles.includes("pre-authorization")) {
+  } else if (profile?.roles.includes("pre-authorization")) {
     return <div>pre-authorization</div>
   } else {
     return <div>error</div>
