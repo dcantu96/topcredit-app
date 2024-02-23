c2665de99224b812802923b6da4ae2ead3e65376
David Cantu <davidcantu966@gmail.com>
2024-02-23 12:03:24 -0600

    Update requests list screen

--- src/routes/requests/list.tsx
+++ src/routes/requests/list.tsx
@@ -1,106 +1,139 @@
-import { useState } from "react"
-import { useRecoilValue } from "recoil"
+import { useRef, useState } from "react"
+import { useRecoilState, useRecoilValue } from "recoil"
 import { AnimatePresence } from "framer-motion"
-import { CheckIcon, XMarkIcon, EyeIcon } from "@heroicons/react/24/solid"
 
 import { STATES_OF_MEXICO } from "../../constants"
-import { basicDetailsSortedSelector } from "./atoms"
-import { useRequestActions } from "./actions"
+import { basicDetailsSortOrderAtom, basicDetailsSortedSelector } from "./atoms"
+import { useNavigate } from "react-router-dom"
+import { ChevronUpDownIcon } from "@heroicons/react/16/solid"
+import DropdownList from "components/atoms/dropdown-list"
+import { ChevronRightIcon } from "@heroicons/react/24/outline"
+import NavLink from "components/atoms/nav-link"
 
-import ButtonLink from "components/atoms/button-link"
-import Table from "components/organisms/table"
-import Button from "components/atoms/button"
-import Dialog from "components/molecules/dialog"
+const SORT_ORDER = [
+  { label: "Más recientes", value: "desc" },
+  { label: "Más antiguas", value: "asc" },
+] as const
 
 const Screen = () => {
+  const navigate = useNavigate()
+  const sortButtonRef = useRef<HTMLButtonElement>(null)
+  const [isSortDropdownOpen, setIsSortDropdownOpen] = useState(false)
+  const toggleSortDropdown = () => setIsSortDropdownOpen((prev) => !prev)
   const basicDetails = useRecoilValue(basicDetailsSortedSelector)
-  return (
-    <div className="inline-block">
-      <div className="flex justify-between mb-2">
-        <h2 className="text-lg font-bold">Gestión de Solicitudes</h2>
-      </div>
-      <Table>
-        <Table.Header
-          columns={["Nombre", "Nómina", "Estado", "Fecha", "Acciones"]}
-        />
-        <tbody className="bg-white">
-          {basicDetails.map((details) => (
-            <tr key={details.id}>
-              <td className="border-b border-slate-100 p-4 pl-8 text-slate-500">
-                {details.firstName} {details.lastName}
-              </td>
-              <td className="border-b border-slate-100 p-4 text-slate-500">
-                {details.employeeNumber}
-              </td>
-              <td className="border-b border-slate-100 p-4 pr-8 text-slate-500">
-                {
-                  STATES_OF_MEXICO.find(
-                    (state) => state.value === details.state,
-                  )?.label
-                }
-              </td>
-              <td className="border-b border-slate-100 p-4 pr-8 text-slate-500">
-                {new Date(details.createdAt).toLocaleDateString()}
-              </td>
-              <td className="border-b border-slate-100 p-4 pr-8 text-slate-500 flex gap-2">
-                <ApproveRequestButton id={details.id} />
-                <DenyRequestButton id={details.id} />
-                <ButtonLink
-                  size="sm"
-                  status="secondary"
-                  to={details.id.toString()}
-                >
-                  <EyeIcon className="w-4 h-4 text-gray-600 p-0" />
-                </ButtonLink>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </div>
+  const [selectedSortOrder, setSelectedSortOrder] = useRecoilState(
+    basicDetailsSortOrderAtom,
   )
-}
-
-export default Screen
-
-const DenyRequestButton = ({ id }: { id: number }) => {
-  const { denyUser } = useRequestActions(id)
-  const [isModalOpen, setModalOpen] = useState(false)
-  const showModal = () => setModalOpen(true)
-
-  const handleDeny = () => {
-    denyUser()
-    setModalOpen(false)
-  }
+  const selectedOrderLabel = SORT_ORDER.find(
+    (order) => order.value === selectedSortOrder,
+  )?.label
 
   return (
     <>
-      <Button size="sm" variant="danger" onClick={showModal}>
-        <XMarkIcon className="w-4 h-4 text-white p-0" />
-      </Button>
-      <AnimatePresence>
-        {isModalOpen && (
-          <Dialog
-            onClose={handleDeny}
-            onCancel={() => setModalOpen(false)}
-            type="danger"
-            title="Rechazar solicitud"
-            message="¿Estás seguro de que deseas rechazar esta solicitud? Esto va a cancelar el proceso de registro del usuario."
-            confirmText="Rechazar"
-            cancelText="Cancelar"
-          />
-        )}
-      </AnimatePresence>
+      {/* pending request */}
+      <div className="flex-1 flex-shrink-0">
+        <header className="py-4 px-4 sm:px-6 lg:px-8 border-gray-900/10 border-b flex items-center justify-between">
+          <h2 className="text-gray-900 leading-7 font-semibold text-base">
+            Solicitudes Pendientes
+          </h2>
+          <div className="relative">
+            <button
+              ref={sortButtonRef}
+              onClick={toggleSortDropdown}
+              className="btn btn-small btn-transparent group text-gray-900 leading-7 text-sm font-medium"
+            >
+              {selectedOrderLabel ?? "Ordenar por"}
+              <ChevronUpDownIcon className="w-6 h-6 text-gray-400" />
+            </button>
+            <AnimatePresence>
+              {isSortDropdownOpen && (
+                <DropdownList
+                  onClickOutside={(e) =>
+                    e.target !== sortButtonRef.current &&
+                    setIsSortDropdownOpen(false)
+                  }
+                >
+                  {SORT_ORDER.map((order) => (
+                    <DropdownList.Item
+                      key={order.value}
+                      text={order.label}
+                      onClick={() => {
+                        setSelectedSortOrder((prev) =>
+                          prev === order.value ? undefined : order.value,
+                        )
+                        setIsSortDropdownOpen(false)
+                      }}
+                    />
+                  ))}
+                </DropdownList>
+              )}
+            </AnimatePresence>
+          </div>
+        </header>
+        <ul className="list-none m-0 p-0" role="list">
+          {basicDetails.map((details) => (
+            <li
+              key={details.id}
+              className="py-4 px-4 sm:px-6 lg:px-8 items-center flex relative border-b border-gray-900/10"
+            >
+              <div className="flex-1 min-w-0">
+                <div className="flex items-center gap-x-3">
+                  <div className="bg-gray-100 p-1 rounded-full flex-none shadow-sm">
+                    <div className="w-2 h-2 bg-gray-500 rounded-full"></div>
+                  </div>
+                  <h2 className="text-gray-900 leading-6 font-semibold text-sm min-w-0">
+                    <a className="flex text-inherit decoration-inherit gap-x-2">
+                      <span className="overflow-ellipsis overflow-hidden whitespace-nowrap">
+                        {details.firstName} {details.lastName}
+                      </span>
+                      <span className="text-gray-400">/</span>
+                      <span className="whitespace-nowrap">
+                        {details.email.split("@")[1]}
+                      </span>
+                    </a>
+                  </h2>
+                </div>
+                <div className="mt-3 flex items-center gap-x-[0.625rem] text-xs leading-5 text-gray-400">
+                  <p className="whitespace-nowrap">
+                    {
+                      STATES_OF_MEXICO.find((s) => s.value === details.state)
+                        ?.label
+                    }
+                  </p>
+                  <svg
+                    viewBox="0 0 2 2"
+                    className="fill-[#d1d5db] flex-none w-[0.125rem] h-[0.125rem]"
+                  >
+                    <circle cx="1" cy="1" r="1"></circle>
+                  </svg>
+                  <p className="whitespace-nowrap">
+                    {new Date(details.createdAt).toLocaleDateString()}
+                  </p>
+                </div>
+              </div>
+              <button
+                onClick={() => navigate("requests/" + details.id)}
+                className="btn btn-small btn-transparent group text-gray-900 leading-7 text-sm font-medium"
+              >
+                <ChevronRightIcon className="w-6 h-6 text-gray-400" />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* activity */}
+      <aside className="bg-slate-50 lg:border-gray-900/10 lg:border-l w-full lg:w-96 lg:top-16 lg:h-full h-fit">
+        <header className="py-4 px-4 sm:px-6 lg:px-8 min-h-[70px] border-gray-900/10 border-b flex items-center">
+          <div className="flex items-baseline justify-between flex-1">
+            <h2 className="text-gray-900 leading-7 font-semibold text-base">
+              Actividad
+            </h2>
+            <NavLink to="/requests">Ver todo</NavLink>
+          </div>
+        </header>
+      </aside>
     </>
   )
 }
 
-const ApproveRequestButton = ({ id }: { id: number }) => {
-  const { approveUser } = useRequestActions(id)
-
-  return (
-    <Button size="sm" onClick={approveUser}>
-      <CheckIcon className="w-4 h-4 text-white p-0" />
-    </Button>
-  )
-}
+export default Screen
