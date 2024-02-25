af418a4cca4d5052246ddba585d78284c78df665
David Cantu <davidcantu966@gmail.com>
2024-02-24 19:02:37 -0600

    Add SmallDot component

---
+++ src/components/atoms/small-dot/small-dot.tsx
@@ -0,0 +1,10 @@
+const SmallDot = () => (
+  <svg
+    viewBox="0 0 2 2"
+    className="fill-[#d1d5db] flex-none w-[0.125rem] h-[0.125rem]"
+  >
+    <circle cx="1" cy="1" r="1"></circle>
+  </svg>
+)
+
+export default SmallDot
