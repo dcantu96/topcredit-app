8166a8989e25fa4bfd5da107a7877b08bf7feaea
David Cantu <davidcantu966@gmail.com>
2024-02-25 01:11:25 -0600

    Add Chip component

---
+++ src/components/atoms/chip/chip.tsx
@@ -0,0 +1,12 @@
+interface ChipProps {
+  children?: React.ReactNode
+  status?: "success" | "warning" | "error" | "info"
+}
+
+const Chip = ({ children, status }: ChipProps) => (
+  <span className="whitespace-nowrap rounded-full py-1 px-2 text-xs bg-indigo-400/10 text-indigo-400 border-indigo-400 border">
+    {children}
+  </span>
+)
+
+export default Chip
