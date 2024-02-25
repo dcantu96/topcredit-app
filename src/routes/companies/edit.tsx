13a01b1ecf6c125c122c21093a27033a39658714
David Cantu <davidcantu966@gmail.com>
2024-02-25 14:39:18 -0600

    Refactor form layout and update header in EditCompany component

--- src/routes/companies/edit.tsx
+++ src/routes/companies/edit.tsx
@@ -1,18 +1,22 @@
 import { useState } from "react"
 import { useRecoilRefresher_UNSTABLE, useRecoilValue } from "recoil"
+import { useNavigate, useParams } from "react-router-dom"
+
+import { useFormErrors } from "hooks/useFormErrors"
 import Input from "components/atoms/input"
 import Button from "components/atoms/button"
-import { useNavigate, useParams } from "react-router-dom"
-import { companySelectorQuery, companyState } from "./loader"
 import ButtonLink from "components/atoms/button-link"
-import { useFormErrors } from "hooks/useFormErrors"
-import { useCompanyActions } from "./actions"
 import Chip from "components/atoms/chip"
-import { DURATION_TYPES } from "../../constants"
 import Label from "components/atoms/label"
+import useToast from "components/providers/toaster/useToast"
+import FormHeader from "components/atoms/layout/form-header"
+import FormContainer from "components/atoms/layout/form-container"
+
+import { useCompanyActions } from "./actions"
+import { companySelectorQuery, companyState } from "./loader"
 import { NewTermForm } from "./new-term-form"
 import AssignTermForm from "./assign-term-form"
-import useToast from "components/providers/toaster/useToast"
+import { DURATION_TYPES } from "../../constants"
 
 const EditCompany = () => {
   const { id } = useParams()
@@ -51,46 +55,40 @@ const EditCompany = () => {
   }
 
   return (
-    <div className="mx-auto px-4 pt-4 max-w-screen-lg">
-      <div className="lg:flex lg:items-center lg:justify-between mb-4 w-full">
-        <div className="min-w-0 flex-1">
-          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-            Editar Cliente
-          </h2>
-        </div>
-        <div className="mt-5 flex lg:ml-4 lg:mt-0">
-          <span className="flex gap-2">
-            <ButtonLink to=".." status="secondary">
-              Ver
-            </ButtonLink>
-            <ButtonLink to="../.." status="secondary">
-              <svg
-                viewBox="0 0 24 24"
-                className="h-5 w-5 text-gray-400 mr-1.5"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
+    <FormContainer>
+      <FormHeader>
+        <FormHeader.Title text="Editar Cliente" />
+        <FormHeader.Actions>
+          <ButtonLink to=".." status="secondary">
+            Ver
+          </ButtonLink>
+          <ButtonLink to="../.." status="secondary">
+            <svg
+              viewBox="0 0 24 24"
+              className="h-5 w-5 text-gray-400 mr-1.5"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <path
+                  d="M3 9L21 9M12 9V20M6.2 20H17.8C18.9201 20 19.4802 20 19.908 19.782C20.2843 19.5903 20.5903 19.2843 20.782 18.908C21 18.4802 21 17.9201 21 16.8V7.2C21 6.0799 21 5.51984 20.782 5.09202C20.5903 4.71569 20.2843 4.40973 19.908 4.21799C19.4802 4 18.9201 4 17.8 4H6.2C5.0799 4 4.51984 4 4.09202 4.21799C3.71569 4.40973 3.40973 4.71569 3.21799 5.09202C3 5.51984 3 6.07989 3 7.2V16.8C3 17.9201 3 18.4802 3.21799 18.908C3.40973 19.2843 3.71569 19.5903 4.09202 19.782C4.51984 20 5.07989 20 6.2 20Z"
+                  stroke="currentColor"
+                  strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                ></g>
-                <g id="SVGRepo_iconCarrier">
-                  <path
-                    d="M3 9L21 9M12 9V20M6.2 20H17.8C18.9201 20 19.4802 20 19.908 19.782C20.2843 19.5903 20.5903 19.2843 20.782 18.908C21 18.4802 21 17.9201 21 16.8V7.2C21 6.0799 21 5.51984 20.782 5.09202C20.5903 4.71569 20.2843 4.40973 19.908 4.21799C19.4802 4 18.9201 4 17.8 4H6.2C5.0799 4 4.51984 4 4.09202 4.21799C3.71569 4.40973 3.40973 4.71569 3.21799 5.09202C3 5.51984 3 6.07989 3 7.2V16.8C3 17.9201 3 18.4802 3.21799 18.908C3.40973 19.2843 3.71569 19.5903 4.09202 19.782C4.51984 20 5.07989 20 6.2 20Z"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></path>
-                </g>
-              </svg>
-              Lista
-            </ButtonLink>
-          </span>
-        </div>
-      </div>
+                ></path>
+              </g>
+            </svg>
+            Lista
+          </ButtonLink>
+        </FormHeader.Actions>
+      </FormHeader>
       <form
         className="w-full grid grid-cols-2 gap-x-4 gap-y-0"
         onSubmit={handleUpdate}
@@ -152,7 +150,7 @@ const EditCompany = () => {
           </Button>
         </div>
       </form>
-    </div>
+    </FormContainer>
   )
 }
 
