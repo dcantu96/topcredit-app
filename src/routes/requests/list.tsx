3bc507be9bfb6bb0af4a4e23f5b342dc9990a70d
David Cantu <davidcantu966@gmail.com>
2024-02-25 14:40:47 -0600

    Add components for list layout and header

--- src/routes/requests/list.tsx
+++ src/routes/requests/list.tsx
@@ -3,11 +3,14 @@ import { useNavigate } from "react-router-dom"
 import { ChevronRightIcon } from "@heroicons/react/24/outline"
 
 import NavLink from "components/atoms/nav-link"
+import SmallDot from "components/atoms/small-dot"
+import ListContainer from "components/atoms/layout/list-container"
+import ListHeader from "components/atoms/layout/list-header"
+import List from "components/atoms/list"
 import ListSortOrderHandler from "components/organisms/list-sort-order-handler"
 
 import { STATES_OF_MEXICO } from "../../constants"
 import { basicDetailsSortedSelector } from "./atoms"
-import SmallDot from "components/atoms/small-dot"
 
 const Screen = () => {
   const navigate = useNavigate()
@@ -16,19 +19,16 @@ const Screen = () => {
   return (
     <>
       {/* pending request */}
-      <div className="flex-1 flex-shrink-0">
-        <header className="py-4 px-4 sm:px-6 lg:px-8 border-gray-900/10 border-b flex items-center justify-between">
-          <h2 className="text-gray-900 leading-7 font-semibold text-base">
-            Solicitudes Pendientes
-          </h2>
-          <ListSortOrderHandler listName="requests" />
-        </header>
-        <ul className="list-none m-0 p-0" role="list">
+      <ListContainer>
+        <ListHeader>
+          <ListHeader.Title text="Solicitudes Pendientes" />
+          <ListHeader.Actions>
+            <ListSortOrderHandler listName="requests" />
+          </ListHeader.Actions>
+        </ListHeader>
+        <List>
           {basicDetails.map((details) => (
-            <li
-              key={details.id}
-              className="py-4 px-4 sm:px-6 lg:px-8 items-center flex relative border-b border-gray-900/10"
-            >
+            <List.Item key={details.id}>
               <div className="flex-1 min-w-0">
                 <div className="flex items-center gap-x-3">
                   <div className="bg-gray-100 p-1 rounded-full flex-none shadow-sm">
@@ -65,10 +65,10 @@ const Screen = () => {
               >
                 <ChevronRightIcon className="w-6 h-6 text-gray-400" />
               </button>
-            </li>
+            </List.Item>
           ))}
-        </ul>
-      </div>
+        </List>
+      </ListContainer>
       {/* activity */}
       <aside className="bg-slate-50 lg:border-gray-900/10 lg:border-l w-full lg:w-96 lg:top-16 lg:h-full h-fit">
         <header className="py-4 px-4 sm:px-6 lg:px-8 min-h-[70px] border-gray-900/10 border-b flex items-center">
