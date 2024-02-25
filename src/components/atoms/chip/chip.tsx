0575a9dbaf2dce8a26994afc7ea523eca06c2b27
David Cantu <davidcantu966@gmail.com>
2024-02-25 16:03:53 -0600

    Remove unused status prop from Chip component

--- src/components/atoms/chip/chip.tsx
+++ src/components/atoms/chip/chip.tsx
@@ -3,7 +3,7 @@ interface ChipProps {
   status?: "success" | "warning" | "error" | "info"
 }
 
-const Chip = ({ children, status }: ChipProps) => (
+const Chip = ({ children }: ChipProps) => (
   <span className="whitespace-nowrap rounded-full py-1 px-2 text-xs bg-indigo-400/10 text-indigo-400 border-indigo-400 border">
     {children}
   </span>
