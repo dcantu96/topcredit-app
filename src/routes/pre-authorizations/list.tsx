ec584727cfbe99406c6d63481e0e49bd6b0fd428
David Cantu <davidcantu966@gmail.com>
2024-02-25 18:26:28 -0600

    Refactor pre-authorization list to use ApprovedUserItem component

--- src/routes/pre-authorizations/list.tsx
+++ src/routes/pre-authorizations/list.tsx
@@ -7,8 +7,9 @@ import Button from "components/atoms/button"
 import ListContainer from "components/atoms/layout/list-container"
 import ListHeader from "components/atoms/layout/list-header"
 import List from "components/atoms/list"
+import Input from "components/atoms/input"
 
-import { approvedUsersSortedSelector } from "./atoms"
+import { ApprovedUsersResponse, approvedUsersSortedSelector } from "./atoms"
 import { MXNFormat } from "../../constants"
 
 const Screen = () => {
@@ -26,69 +27,7 @@ const Screen = () => {
         </ListHeader>
         <List>
           {approvedUsers.map((user) => (
-            <List.Item key={user.id}>
-              <div className="flex-1 min-w-0">
-                <div className="flex items-center gap-x-3">
-                  <div className="bg-gray-100 p-1 rounded-full flex-none shadow-sm">
-                    <div className="w-2 h-2 bg-gray-500 rounded-full"></div>
-                  </div>
-                  <h2 className="text-gray-900 leading-6 font-semibold text-sm min-w-0">
-                    <a className="flex text-inherit decoration-inherit gap-x-2">
-                      <span className="overflow-ellipsis overflow-hidden whitespace-nowrap">
-                        {user.firstName} {user.lastName}
-                      </span>
-                      <span className="text-gray-400">/</span>
-                      <span className="whitespace-nowrap">
-                        {new Date(user.createdAt).toLocaleDateString()}
-                      </span>
-                    </a>
-                  </h2>
-                </div>
-                <div className="mt-3 flex items-center gap-x-[0.625rem] text-xs leading-5 text-gray-400">
-                  <p className="whitespace-nowrap">{user.employeeNumber}</p>
-                  <SmallDot />
-                  <p className="whitespace-nowrap">
-                    {user.email.split("@")[1]}
-                  </p>
-                </div>
-              </div>
-              <div className="flex-1 min-w-0 flex justify-between">
-                <div>
-                  <div className="flex items-center gap-x-3">
-                    <h2 className="text-gray-900 leading-6 font-semibold text-sm min-w-0">
-                      <a className="flex text-inherit decoration-inherit gap-x-2">
-                        <span className="overflow-ellipsis overflow-hidden whitespace-nowrap">
-                          {user.salary ? MXNFormat.format(user.salary) : 0}
-                        </span>
-                        <span className="text-gray-400">/</span>
-                        <span className="whitespace-nowrap">
-                          {user.salaryFrequency === "Q"
-                            ? "Quincenales"
-                            : "Mensuales"}
-                        </span>
-                      </a>
-                    </h2>
-                  </div>
-                  <div className="mt-3 flex items-center gap-x-[0.625rem] text-xs leading-5 text-gray-400">
-                    <p className="whitespace-nowrap">
-                      {user.salary ? MXNFormat.format(user.salary) : 0}
-                    </p>
-                    <SmallDot />
-                    <p className="whitespace-nowrap">
-                      {user.salaryFrequency === "Q"
-                        ? "Quincenales"
-                        : "Mensuales"}
-                    </p>
-                  </div>
-                </div>
-                <div className="flex items-center gap-4">
-                  <Button status="primary" disabled>
-                    Autorizar
-                  </Button>
-                  <Button status="secondary">Rechazar</Button>
-                </div>
-              </div>
-            </List.Item>
+            <ApprovedUserItem key={user.id} user={user} />
           ))}
         </List>
       </ListContainer>
@@ -104,6 +43,74 @@ const Screen = () => {
         </header>
       </aside>
     </>
+  )
+}
+
+interface ApprovedUserItemProps {
+  user: ApprovedUsersResponse
+}
+
+const ApprovedUserItem = ({ user }: ApprovedUserItemProps) => {
+  return (
+    <List.Item>
+      <div className="min-w-80">
+        <div className="flex items-center gap-x-3">
+          <div className="bg-gray-100 p-1 rounded-full flex-none shadow-sm">
+            <div className="w-2 h-2 bg-gray-500 rounded-full"></div>
+          </div>
+          <h2 className="text-gray-900 leading-6 font-semibold text-sm min-w-0 flex text-inherit decoration-inherit gap-x-2">
+            <span className="overflow-ellipsis overflow-hidden whitespace-nowrap">
+              {user.firstName} {user.lastName}
+            </span>
+            <span className="text-gray-400">/</span>
+            <span className="whitespace-nowrap">
+              {new Date(user.createdAt).toLocaleDateString()}
+            </span>
+          </h2>
+        </div>
+        <div className="flex items-center gap-x-3 mt-1">
+          <p className="text-gray-500 leading-6 font-medium text-sm min-w-0 flex text-inherit gap-x-2">
+            <span className="overflow-ellipsis overflow-hidden whitespace-nowrap">
+              {user.salary ? MXNFormat.format(user.salary) : 0}
+            </span>
+            <span className="text-gray-400">/</span>
+            <span className="whitespace-nowrap">
+              {user.salaryFrequency === "Q" ? "Quincenales" : "Mensuales"}
+            </span>
+          </p>
+        </div>
+        <div className="flex items-center gap-x-3 text-xs leading-5 text-gray-500">
+          <p className="whitespace-nowrap">{user.employeeNumber}</p>
+          <SmallDot />
+          <p className="whitespace-nowrap">{user.email.split("@")[1]}</p>
+        </div>
+      </div>
+      <div className="flex-1 min-w-96 flex justify-between gap-4">
+        <div>
+          <Input
+            id="loan-amount"
+            value="1000"
+            onChange={() => {}}
+            type="number"
+            prefix="$"
+            label="Préstamo"
+            placeholder="10000"
+            trailingDropdownId="loan-amount-frequency"
+            trailingDropdownLabel="Frecuencia de pago"
+            trailingDropdownOptions={[
+              { label: "10 Meses", value: "10 Meses" },
+              { label: "12 Meses", value: "12 Meses" },
+            ]}
+          />
+        </div>
+        <div className="flex items-center gap-4">
+          <Button status="primary" disabled>
+            Autorizar
+          </Button>
+          <Button status="secondary">Rechazar</Button>
+        </div>
+      </div>
+    </List.Item>
   )
 }
 
