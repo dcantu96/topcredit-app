264184aa36547318898b75bc31f5867eb09afb30
David Cantu <davidcantu966@gmail.com>
2024-02-28 19:59:36 -0600

    Add PendingAuthorizations component to Dashboard

--- src/routes/dashboards/index.tsx
+++ src/routes/dashboards/index.tsx
@@ -6,6 +6,9 @@ const StaffRequests = lazy(() => import("../../routes/requests/list"))
 const PreAuthorizations = lazy(
   () => import("../../routes/pre-authorizations/list"),
 )
+const PendingAuthorizations = lazy(
+  () => import("../../routes/pending-authorizations/list"),
+)
 
 /**
  * This component is in charge of rendering a screen depending on the user's role.
@@ -19,6 +22,8 @@ const Dashboard = () => {
     return <StaffRequests />
   } else if (profile?.roles.includes("pre_authorizations")) {
     return <PreAuthorizations />
+  } else if (profile?.roles.includes("authorizations")) {
+    return <PendingAuthorizations />
   } else {
     return <div>error</div>
   }
