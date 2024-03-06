98f89791835bab8435d468a8e4a600c27603c43c
David Cantu <davidcantu966@gmail.com>
2024-03-05 23:00:39 -0600

    feat: add props so viewer supports new file props

--- src/components/atoms/file-viewer/file-viewer.tsx
+++ src/components/atoms/file-viewer/file-viewer.tsx
@@ -1,10 +1,14 @@
-import { DocumentIcon, EllipsisHorizontalIcon } from "@heroicons/react/24/solid"
+import { TrashIcon } from "@heroicons/react/24/outline"
+import { DocumentIcon } from "@heroicons/react/24/solid"
 
 interface FileViewerProps {
   label: string
   fileName?: string
   fileDate?: string
   fileSize?: string
+  contentType?: string
+  fileUrl?: string
+  onDelete?: () => void
 }
 
 const FileViewer = ({
@@ -12,24 +16,46 @@ const FileViewer = ({
   fileDate = "-",
   fileName = "-",
   fileSize = "-",
+  contentType,
+  fileUrl,
+  onDelete,
 }: FileViewerProps) => {
   return (
     <>
       <label className="text-gray-900 font-medium leading-6 text-sm">
         {label}
       </label>
-      <div className="ring-1 ring-gray-300 rounded-md w-full p-4 mt-2">
+      <div
+        className="ring-1 ring-gray-300 rounded-md w-full p-4 mt-2"
+        onClick={() => {
+          if (!fileUrl) return
+          window.location.href = fileUrl
+        }}
+      >
         <div className="flex justify-between w-full mb-4">
           <DocumentIcon className="h-5 w-5 text-gray-500" />
-          <EllipsisHorizontalIcon className="h-5 w-5 text-gray-500" />
+
+          <TrashIcon
+            onClick={(e) => {
+              console.log("click")
+              e.stopPropagation()
+              onDelete && onDelete()
+            }}
+            className="h-5 w-5 text-red-500"
+          />
         </div>
         <p className="text-gray-900 font-medium mb-2">{fileName}</p>
         <p className="text-gray-500 text-sm">
-          {fileDate} &#x2022; {fileSize}
+          {new Date(fileDate).toLocaleDateString()} &#x2022;{" "}
+          {bytesToMb(Number(fileSize))} &#x2022; {contentType}
         </p>
       </div>
     </>
   )
+}
+
+const bytesToMb = (bytes: number) => {
+  return (bytes / (1024 * 1024)).toFixed(2) + "MB"
 }
 
 export default FileViewer
