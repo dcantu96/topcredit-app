9950007edc57ccc3f7ba0b25db93d2e51f9957e9
David Cantu <davidcantu966@gmail.com>
2024-04-06 15:52:46 -0600

    fix: add status to chip

--- src/components/atoms/chip/chip.tsx
+++ src/components/atoms/chip/chip.tsx
@@ -3,10 +3,20 @@ interface ChipProps {
   status?: "success" | "warning" | "error" | "info"
 }
 
-const Chip = ({ children }: ChipProps) => (
-  <span className="whitespace-nowrap rounded-full py-1 px-2 text-xs bg-indigo-400/10 text-indigo-400 border-indigo-400 border">
-    {children}
-  </span>
-)
+const Chip = ({ children, status = "info" }: ChipProps) => {
+  const statusClasses: Record<NonNullable<ChipProps["status"]>, string> = {
+    success: "bg-green-400/10 text-green-400 border-green-400",
+    warning: "bg-yellow-400/10 text-yellow-400 border-yellow-400",
+    error: "bg-red-500/10 text-red-500 border-red-500",
+    info: "bg-indigo-400/10 text-indigo-400 border-indigo-400",
+  }
+  return (
+    <span
+      className={`whitespace-nowrap rounded-full py-1 px-2 text-xs ${statusClasses[status]} border`}
+    >
+      {children}
+    </span>
+  )
+}
 
 export default Chip
