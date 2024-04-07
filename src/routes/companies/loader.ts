7c164a2b69ebd52eef7e15a6fc37486e8dc933f0
David Cantu <davidcantu966@gmail.com>
2024-04-07 12:54:07 -0600

    fix: new companiesDataSelector selector

--- src/routes/companies/loader.ts
+++ src/routes/companies/loader.ts
@@ -3,8 +3,8 @@ import { atom, atomFamily, selector, selectorFamily } from "recoil"
 import { Company, Term } from "src/schema.types"
 
 export interface CompanyWithTermsResponse extends Omit<Company, "terms"> {
-  terms: {
-    data: Term[]
+  terms?: {
+    data?: Term[]
   }
 }
 
@@ -12,16 +12,20 @@ export const companiesSelectorQuery = selector<Company[]>({
   key: "companiesSelectorQuery",
   get: async ({ get }) => {
     const api = get(apiSelector)
-    const { data } = await api.get("companies", {
-      params: {
-        include: "terms",
+    const { data }: { data: CompanyWithTermsResponse[] } = await api.get(
+      "companies",
+      {
+        params: {
+          include: "terms",
+        },
       },
-    })
-    return data.map((company: CompanyWithTermsResponse) => ({
+    )
+    return data.map((company) => ({
       ...company,
-      terms: company.terms.data.map((term) => ({
-        ...term,
-      })),
+      terms:
+        company.terms?.data?.map((term) => ({
+          ...term,
+        })) ?? [],
     }))
   },
 })
@@ -47,9 +51,10 @@ export const companySelectorQuery = selectorFamily<Company, string>({
       )
       return {
         ...data,
-        terms: data.terms.data.map((term) => ({
-          ...term,
-        })),
+        terms:
+          data.terms?.data?.map((term) => ({
+            ...term,
+          })) ?? [],
       }
     },
 })
@@ -57,4 +62,42 @@ export const companySelectorQuery = selectorFamily<Company, string>({
 export const companyState = atomFamily({
   key: "companyState",
   default: companySelectorQuery,
+})
+
+type CompanyFilters = {
+  domain?: string
+  name?: string
+  rate?: number
+  borrowingCapacity?: number | null
+  employeeSalaryFrequency?: "biweekly" | "monthly"
+}
+
+type CompanyDataParams = {
+  filter?: CompanyFilters
+  include?: string
+  fields?: string
+}
+
+export const companiesDataSelector = selectorFamily<
+  Company[],
+  CompanyDataParams
+>({
+  key: "companyDataSelector",
+  get:
+    (params) =>
+    async ({ get }) => {
+      const api = get(apiSelector)
+      const { data }: { data: CompanyWithTermsResponse[] } = await api.get(
+        `companies`,
+        { params },
+      )
+      console.log(data)
+      return data.map((company) => ({
+        ...company,
+        terms:
+          company.terms?.data?.map((term) => ({
+            ...term,
+          })) ?? [],
+      }))
+    },
 })
