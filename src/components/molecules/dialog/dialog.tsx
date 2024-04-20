c8431e08cc4f7b96c24d5888dcc634bb52c4354c
David Cantu <davidcantu966@gmail.com>
2024-04-20 15:54:39 -0600

    fix: add z index to dialog

--- src/components/molecules/dialog/dialog.tsx
+++ src/components/molecules/dialog/dialog.tsx
@@ -33,7 +33,7 @@ const Dialog = ({ message, title, ...rest }: DialogProps) => {
   return (
     <motion.div
       onClick={(e) => e.stopPropagation()}
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
       exit={{ opacity: 0 }}
     >
       <motion.div
