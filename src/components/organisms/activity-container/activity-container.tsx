162528e6300930be8567685af689aa6ddb4796e2
David Cantu <davidcantu966@gmail.com>
2024-05-01 16:20:12 -0600

    fix: activity monitor header padding

--- src/components/organisms/activity-container/activity-container.tsx
+++ src/components/organisms/activity-container/activity-container.tsx
@@ -10,7 +10,7 @@ const ActivityContainer = ({
 }: ActivityContainerProps) => {
   return (
     <aside className="bg-slate-50 lg:border-gray-900/10 lg:border-l w-full lg:w-96 lg:top-16 lg:h-full h-fit">
-      <header className="py-4 px-4 sm:px-6 lg:px-8 min-h-[70px] border-gray-900/10 border-b flex items-center">
+      <header className="py-2 sm:py-4 px-4 sm:px-6 lg:px-8 min-h-[70px] border-gray-900/10 border-b flex items-center">
         <div className="flex items-baseline justify-between flex-1">
           <h2 className="text-gray-900 leading-7 font-semibold text-base">
             Actividad
