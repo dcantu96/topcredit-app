0f849246cfced9449ed15e614487e1ff227c5924
David Cantu <davidcantu966@gmail.com>
2025-02-27 15:16:42 -0600

    feat: add navigation for payments role in dashboard routing

--- src/routes/dashboards/index.tsx
+++ src/routes/dashboards/index.tsx
@@ -19,6 +19,8 @@ const Dashboard = () => {
     return <Navigate to="dispersions" />
   } else if (profile?.roles.includes("hr") && profile.hrCompanyId) {
     return <Navigate to={`hr/${profile.hrCompanyId}`} />
+  } else if (profile?.roles.includes("payments")) {
+    return <Navigate to={`payments`} />
   } else {
     return <div>dashboard not found</div>
   }
